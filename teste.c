3d1612a0c38bc83c46382f24a366755af9300c6f
raffaelnagel <raffael.nagel@gmail.com>
2014-04-07 16:43:59 -0300

    working

--- teste.c
+++ teste.c
@@ -16,6 +16,11 @@ typedef struct{
 		int threadCount;
 } function_t;
 
+matrix_t *MA;
+matrix_t *MB;
+matrix_t *MC;
+
+
 void setMatrixValue(matrix_t *mMatrix, int line, int column, int value){
 		mMatrix->matrix[line * 10 + column] = value;
 }
@@ -25,54 +30,87 @@ int getMatrixValue(matrix_t *mMatrix, int line, int column){
 }
 
 matrix_t *readMatrix(char *filename){
+		FILE *file = NULL;
 		matrix_t *mMatrix = calloc(1, sizeof(matrix_t));
+		char strLinha[1048576];
+		char strNumero[1048576]; 
 		int lines, columns;
 		int i,j, tmp;
 		
-		FILE *input = fopen(filename, "r");
+		file = fopen(filename, "r");
 		
-		if(input == NULL){
+		if(file == NULL){
 			printf("File Error!");
 			exit(1);
 		}
 		
-		fscanf(input, "LINHAS = %d\n", &lines);
-		fscanf(input, "COLUNAS = %d\n", &columns);
+		//get Linhas
+		fgets(strLinha, sizeof(strLinha), file);
+		for (i = 0; strLinha[i] != '='; i++); 
+		i+=2;//pula o espaço depois de =
+		for (j = 0; ((strLinha[i] != '\n') && (strLinha[i] != ' ') && (strLinha[i] != '\t')); strNumero[j++] = strLinha[i++]);
+		strNumero[j] = '\0';
+		lines = atoi(strNumero);
+		
+		//get Colunas		
+		fgets(strLinha, sizeof(strLinha), file);
+		for (i = 0; strLinha[i] != '='; i++);
+		i+=2;//pula o espaço depois de =
+		for (j = 0; ((strLinha[i] != '\n') && (strLinha[i] != ' ') && (strLinha[i] != '\t')); strNumero[j++] = strLinha[i++]);
+		strNumero[j] = '\0';  
+		columns = atoi(strNumero);
+		
 		
 		mMatrix->lines = lines;
 		mMatrix->columns = columns;
 		mMatrix->matrix = calloc(mMatrix->lines*mMatrix->columns, sizeof(int));
 		
-		for(i = 0; i < lines; ++i){
-			for(j = 0; j < columns; ++j){
-				fscanf(input, "%d", &tmp);
-				setMatrixValue(mMatrix, i, j, tmp);
+		//get values
+		i = 0; //linha
+	    j = 0; //coluna
+	    int k = 0; //iterador da string linha
+	    int l = 0; //iterador da string numero
+	  
+	    while ((fgets(strLinha,sizeof(strLinha),file) != NULL) && (i < lines))
+	    {
+			j = 0;
+			k = 0;		
+			//le numero a numero da linha
+			while ((k < strlen(strLinha)) && (j < columns))
+			{
+				for (l = 0; ((strLinha[k] != '\n') && (strLinha[k] != ' ') && (strLinha[k] != '\t')); strNumero[l++] = strLinha[k++]);
+				strNumero[l] = '\0';
+				k++; //pula o espaço
+		  
+				setMatrixValue(mMatrix, i, j, atoi(strNumero));
+				j++;
 			}
+			i++;
 		}
 		
-		fclose(input);
+		fclose(file);
 		return mMatrix;
 }
 
 void writeMatrix(char *filename, matrix_t mMatrix){
-	FILE *output = fopen(filename, "w+");
+	FILE *file = fopen(filename, "w");
 	int i,j;
 	
-	if(output == NULL){
+	if(file == NULL){
 		printf("File Error!");
 		exit(1);
 	}
 	
-	fprintf(output, "LINHAS = %d\n", mMatrix.lines);
-	fprintf(output, "COLUNAS = %d\n", mMatrix.columns);
+	fprintf(file, "LINHAS = %d\n", mMatrix.lines);
+	fprintf(file, "COLUNAS = %d\n", mMatrix.columns);
 	
 	for(i = 0; i < mMatrix.lines; ++i){
 		for(j = 0; j < mMatrix.columns; ++j){
-			fprintf(output, "%d ", getMatrixValue(&mMatrix, i, j));
+			fprintf(file, "%d ", getMatrixValue(&mMatrix, i, j));
 		}
-		fprintf(output, "\n");
+		fprintf(file, "\n");
 	}
-	fclose(output);
+	fclose(file);
 }
 
 void printMatrix(matrix_t mMatrix){
@@ -142,16 +180,22 @@ matrix_t multiplyMatrix(matrix_t mA, matrix_t mB, int threadsNumber){
 
 int main(int argc, char *argv[]){
 	
-	matrix_t *MA = readMatrix("ini1.txt");	
-	//MB = readMatrix("ini2.txt");
+	MA = readMatrix("ini1.txt");	
+	MB = readMatrix("ini2.txt");
 	
 	////////////////////////////////////////////////////////////////////
 	int threadsNumber = 2;
+	//matrix_t mResult = createResultMatrix(*MA,*MA);
+	//mResult = multiplyMatrix(*MA, *MA, threadsNumber);
 	
-	matrix_t mResult = multiplyMatrix(*MA, *MA, threadsNumber);
+	writeMatrix("out.txt", *MB);
+	printf("\n\nMatriz A \n");
+	printMatrix(*MA);
+	printf("\n\nMatriz B \n");
+	printMatrix(*MB);
 	
-	writeMatrix("out.txt", mResult);
 	
+	printf("\n\nMA[1][4] = %d \n\n", getMatrixValue(MA, 1, 4));
 	
 	return 0;
 }
---
+++ teste2.c
@@ -0,0 +1,259 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#define tamanhoAmostra 10
+
+typedef struct {
+	int id;
+} object;
+
+int readMatrix(const char *filename, int *linesCount, int *columnsCount, int ***resultMatrix)
+{
+  FILE *inputFile = NULL;
+  char stringBuffer[1048576], tmpBuffer[1048576]; 
+  int i,j;
+  int **mMatrix;
+  
+  inputFile = fopen(filename, "r");
+  if (inputFile == NULL){
+    printf("\nFile Error!");
+    return 0;
+  }
+    
+  //get LINHAS
+  fgets(stringBuffer, sizeof(stringBuffer), inputFile);
+  for (i = 0; stringBuffer[i] != '='; i++); 
+  i+=2;
+  for (j = 0; ((stringBuffer[i] != '\n') && (stringBuffer[i] != ' ') && (stringBuffer[i] != '\t')); tmpBuffer[j++] = stringBuffer[i++]);
+  tmpBuffer[j] = '\0';
+  
+  *linesCount = atoi(tmpBuffer);
+ 
+  //get COLUNAS
+  fgets(stringBuffer, sizeof(stringBuffer), inputFile);
+  for (i = 0; stringBuffer[i] != '='; i++);
+  i+=2;
+  for (j = 0; ((stringBuffer[i] != '\n') && (stringBuffer[i] != ' ') && (stringBuffer[i] != '\t')); tmpBuffer[j++] = stringBuffer[i++]);
+  tmpBuffer[j] = '\0';
+  
+  *columnsCount = atoi(tmpBuffer);
+  
+  //Define result matrix
+  mMatrix = NULL;
+  mMatrix = (int **) malloc(sizeof(int *) * (*linesCount));
+  
+  for (i = 0; i < *linesCount; i++){
+    mMatrix[i] = (int *) malloc (sizeof(int) * (*columnsCount));   
+  }
+  
+  //get Values
+  i = 0;
+  j = 0; 
+  int itBuffer = 0, itTmp = 0;
+  
+  while ((fgets(stringBuffer,sizeof(stringBuffer),inputFile) != NULL) && (i < *linesCount)){
+		j = 0;
+		itBuffer = 0;    
+	 
+		while ((itBuffer < strlen(stringBuffer)) && (j < (*columnsCount))){
+			  for (itTmp = 0; ((stringBuffer[itBuffer] != '\n') && (stringBuffer[itBuffer] != ' ') && (stringBuffer[itBuffer] != '\t')); tmpBuffer[itTmp++] = stringBuffer[itBuffer++]);
+			  tmpBuffer[itTmp] = '\0';
+			  itBuffer++;
+			  			  
+			  mMatrix[i][j] = atoi(tmpBuffer);
+			  j++;
+		}		
+		i++;
+  }
+  
+  *resultMatrix = mMatrix;
+    
+  return 1; //ok
+}
+
+int writeMatrix(char* filename, int** matriz, int linesCount, int columnsCount)
+{
+	FILE* outputFile = NULL;
+	int i, j;
+
+	outputFile = fopen(filename, "w");
+	
+	if(outputFile == NULL){
+		printf("File Error!");
+		exit(1);
+	}
+
+	fprintf(outputFile, "LINHAS = %d\n", linesCount);
+	fprintf(outputFile, "COLUNAS = %d\n", columnsCount);
+
+	for(i = 0; i < linesCount; i++)	{
+		for(j = 0; j < columnsCount-1; j++){
+			fprintf(outputFile, "%d ", matriz[i][j]);
+		}
+		fprintf(outputFile, "%d\n", matriz[i][j-1]);
+	}
+
+	fclose(outputFile);
+	return 0;
+}
+
+
+int ** matrixA;         
+int ** matrixB;  
+int ** matrixResult;
+
+int mAlines, mAcolumns;  
+int mBlines, mBcolumns;  
+int mResultlines, mResultcolumns; 
+
+int numThreads;
+
+void printMatrix(int** mMatrix, int lines, int columns);
+
+int  multiplyLineColumn(int* lineValue, int* columnValue, int size);
+
+void getLineValues(int** mat, int numColunas, int indiceLinha, int* out);
+
+void getColumnValues(int** mat, int numLinhas, int indiceColuna, int* out);
+
+void init(int argc, char** argv);
+
+void* worker(void* args);
+
+int main(int argc, char** argv)
+{
+	int i,j;
+	pthread_t* threads;
+	pthread_attr_t pthread_custom_attr;
+	object* obj;
+
+	init(argc,argv);
+
+	//get time here
+
+	printf("starting...\n");	
+	
+	threads = (pthread_t*)malloc(numThreads*sizeof(*threads));
+	pthread_attr_init(&pthread_custom_attr);
+
+	obj = (object*)malloc(numThreads*sizeof(object));
+
+	for(i = 0; i < tamanhoAmostra; i++){		
+		for(j = 0; j < numThreads; j++){
+			obj[j].id = j;	
+			pthread_create(&threads[j],&pthread_custom_attr,worker,(void*)&obj[j]);
+		}	
+		//JOIN
+		for(j = 0; j < numThreads; j++){
+			pthread_join(threads[j],NULL);
+		}
+	}
+    free(obj);
+    
+	//get time here
+	
+	printf("finish\n");
+	
+	//print time diff
+	
+	
+    writeMatrix("out.txt",matrixResult,mResultlines,mResultcolumns);
+
+	return 1;
+}
+
+void printMatrix(int** mMatrix, int lines, int columns){
+  int i, j;
+
+  for(i = 0; i < lines; i++){
+    for(j = 0; j < columns; j++){
+      printf("%d ", mMatrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int multiplyLineColumn(int* lineValue, int* columnValue, int size){
+  int i, result = 0;  
+
+  for(i = 0; i < size; i++){
+    result += lineValue[i]*columnValue[i];
+  }
+
+  return result;
+}
+
+void getLineValues(int** mMatrix, int columns, int index, int* values){
+  int i;
+
+  for(i = 0; i < columns; i++) {
+    values[i] = mMatrix[index][i];
+  }
+}
+
+void getColumnValues(int** mMatrix, int lines, int index, int* values){
+  int i;
+
+  for(i = 0; i < lines; i++){
+    values[i] = mMatrix[i][index];
+  }
+}
+
+void init(int argc, char** argv){
+  int i; 
+  
+  if (argc != 2 || atoi(argv[1]) <= 0)
+	exit(1);  
+  
+  numThreads = atoi(argv[1]);  
+   
+  //Read Files
+  readMatrix("ini1.txt", &mAlines, &mAcolumns, &matrixA);
+  readMatrix("ini2.txt", &mBlines, &mBcolumns, &matrixB);
+  
+  //check
+  if (mAcolumns != mBlines){
+    printf("Can't multiply.\n\n");
+    exit(1);
+  }
+
+  mResultlines = mAlines;
+  mResultcolumns = mBcolumns;
+  matrixResult = (int**)malloc(mResultlines*sizeof(int*));
+
+  for(i = 0; i < mResultlines; i++){
+  	matrixResult[i] = (int*)malloc(mResultcolumns*sizeof(int));
+  }
+  
+  if (numThreads > mResultlines){
+    numThreads = mResultlines;
+  }
+}
+
+void* worker(void *args)
+{
+  int i,j;
+  object *obj=(object *)args; 
+  int threadId = obj->id;
+
+  for(i = threadId; i < mAlines; i += numThreads){    
+	   
+     int* threadLine = (int*) malloc(mAcolumns * sizeof(int));
+     getLineValues(matrixA, mAcolumns, i, threadLine);
+     
+     for(j = 0; j < mBcolumns; j++){
+       int* threadColumn = (int*) malloc(mBlines * sizeof(int));
+       
+       getColumnValues(matrixB,mBlines,j,threadColumn);
+       
+       matrixResult[i][j] = multiplyLineColumn(threadLine, threadColumn, mAcolumns);
+     }
+  }
+
+  return (NULL);
+}
+
+
